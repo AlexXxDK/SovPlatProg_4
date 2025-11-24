6d6c5a1ddb12939c587a20ee51a8a0023d6ab6b3
Trakt <traktor690001@mail.ru>
2025-11-24 15:15:05 +0300

    Hotfix: сохранение TODO в файл (фикс потери данных)

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 todos = ["Что-то сделать"]
 print(f"Текущие задачи: {todos}")
 
+def add_todo(task):
+    todos.append(task)
+add_todo("Что-то сделать 2")
+print(f"Текущие задачи: {todos}")
+
+def add_todo_secure(task, password="secret"):
+    if password == "secret":
+        todos.append(task)
+        print("Задача добавлена!")
+    else:
+        print("Ошибка: неверный пароль")
+add_todo_secure("Сделать отчёт", "secret")
+
 with open('todos.txt', 'w') as f:
     f.write(str(todos))
 print("Задачи сохранены в файл")
