d918729bd9511f52e09741e5997a368b4fc03f25
Trakt <traktor690001@mail.ru>
2025-11-24 14:45:17 +0300

    Feature/user-auth: func security  add_todo_secure

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ def add_todo(task):
     todos.append(task)
 add_todo("Что-то сделать 2")
 print(f"Текущие задачи: {todos}")
+
+def add_todo_secure(task, password="secret"):
+    if password == "secret":
+        todos.append(task)
+        print("Задача добавлена!")
+    else:
+        print("Ошибка: неверный пароль")
+add_todo_secure("Сделать отчёт", "secret")
